44e96a92cbce2900946dea7e926f7b27ed089238
mdt <mailan.dinhthanh@inverso.de>
2018-07-22 10:32:11 +0200

    Added initial SchadenIncident.sol

---
+++ truffle/contracts/SchadenIncident.sol
@@ -0,0 +1,19 @@
+pragma solidity ^0.4.24;
+contract schadenincident {
+ /* Define variable of this contracts attributes*/
+ address liability;
+ uint damageSum;
+
+ /* this function is executed at initialization and sets bikerAdress in the contract */
+
+ constructor(address liabilityAddress) public { 
+    liability = liabilityAddress;
+    damageSum = 500;
+ }
+    function getLiabilityAddress() public constant returns (address) {
+    return liability;
+ }
+    function damageSums() public constant returns (uint) {
+    return damageSum;
+ }
+}s
